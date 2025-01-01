5a3a99fbff2750728658d9c6746e89931f0e97d1
Naima <naima.wahbi92@gmail.com>
2025-01-01 16:27:05 -0500

    Finished Hands-on Lab: Implement API endpoints using Express-Mongo

--- server/database/app.js
+++ server/database/app.js
@@ -70,21 +70,19 @@ app.get('/fetchDealers', async (req, res) => {
 // Express route to fetch Dealers by a particular state
 app.get('/fetchDealers/:state', async (req, res) => {
     //Write your code here  
-    app.get('/fetchDealers/:state', async (req, res) => {
-        try {
-            const documents = await Dealerships.find({ state: req.params.state });
-            res.json(documents);
-        } catch (error) {
-            res.status(500).json({ error: 'Error fetching documents' });
-        }
-    });
+    try {
+        const documents = await Dealerships.find({ state: req.params.state });
+        res.json(documents);
+    } catch (error) {
+        res.status(500).json({ error: 'Error fetching documents' });
+    }
 });
 
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
     //Write your code here
     try {
-        const document = await Dealerships.findById(req.params.id);
+        const document = await Dealerships.find({id : req.params.id});
         if (!document) {
             return res.status(404).json({ error: 'Dealer not found' });
         }
